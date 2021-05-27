b7597961f24323c189c25ae93b67ac151f8fcde9
Stanko Culaja <culaja@gmail.com>
2021-05-27 13:19:13 +0200

    Added cpu temperature fetch.

---
+++ WebApplication/Data/CpuTemperatureService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace WebApplication.Data
+{
+    public sealed class CpuTemperatureService
+    {
+        public decimal? TryRead()
+        {
+            try
+            {
+                return ReadInternal();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                return null;
+            }
+        }
+
+        private decimal? ReadInternal()
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cat",
+                    Arguments = "/sys/class/thermal/thermal_zone0/temp",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return process.ExitCode == 0
+                ? int.Parse(result) / 1000m
+                : null;
+        }
+    }
+}
